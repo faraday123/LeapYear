3ac10fb39366caf4a63c695aca4e5edcbc2d66d4
samuel-adjin <samuel.adjin@amalitech.org>
2021-03-28 09:15:41 +0000

    first commit

---
+++ LeapYear.java
@@ -0,0 +1,21 @@
+
+/**
+ *  Name: Samuel Adams Adjin
+ */
+
+public class isLeapYear {
+
+    public static boolean isLeapYear(int year) {
+        if( !(year >= 1 && year <= 9999)){
+            return false;
+        }else if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
+            return true;
+        }else{
+            return false;
+        }
+
+
+    }
+
+
+}
